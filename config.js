981eeefdf4cd8eec167ee063e6a77e2cbcd4b80e
5mooth0perator <38310568+5mooth0perator@users.noreply.github.com>
2018-04-21 21:48:11 -0300

    coinUnits = 10000

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var api = 'http://35.173.79.208:32348';
 var blockTargetInterval = 240;
-var coinUnits = 10000000;
+var coinUnits = 10000;
 var symbol = 'RHD';
 var refreshDelay = 30000;
 // pools stats by MainCoins
